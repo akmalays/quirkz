2eb2b1f63b25b5941d1822ab70b7e536f8d6babb
akmalays <akmalays@gmail.com>
2023-03-13 21:24:42 +0700

    add progress bar and redirect func

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,25 @@
-import { Grid } from "@mui/material";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Box, Grid, LinearProgress } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import quikzLogo from "../../assets/images/quicks_logo.png";
 
 export default function Home() {
+  const [progress, setProgress] = useState<number>(10);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          navigate("/chat");
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+  }, []);
+
   return (
     <div>
       <Grid sx={{ backgroundColor: "#43B78D", minHeight: "100vh" }}>
@@ -12,10 +30,21 @@ export default function Home() {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            minWidth: "50%",
           }}
         >
           <img alt="quikzImage" src={quikzLogo} />
+        </Grid>
+        <Grid
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            pt: "10vh",
+          }}
+        >
+          <Box sx={{ width: "50%" }}>
+            <LinearProgress variant="determinate" value={progress} />
+          </Box>
         </Grid>
       </Grid>
     </div>
