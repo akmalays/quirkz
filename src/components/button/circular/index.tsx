9562b1f6bad9ca642b544f0c02167a6bcdb08156
akmalays <akmalays@gmail.com>
2023-03-14 10:55:37 +0700

    add circular icon elements

--- src/components/button/circular/index.tsx
+++ src/components/button/circular/index.tsx
@@ -1,3 +1,39 @@
-export default function CircularButton() {
-  return <div></div>;
+import { Button, Icon } from "@mui/material";
+import { StylesSxProps } from "../../../theme/element";
+
+export default function CircularButton(props: any) {
+  const { icon } = props;
+
+  //button style
+  const styles: StylesSxProps = {
+    button: {
+      alignItems: "center",
+      width: "68px",
+      height: "68px",
+      textTransform: "none",
+      backgroundColor: "#2F80ED",
+      color: "white",
+      cursor: "pointer",
+      borderRadius: 30,
+      "&:hover": {
+        backgroundColor: "#215B9D",
+        color: "white",
+      },
+    },
+  };
+  const circularIcon = () => {
+    return (
+      <div>
+        <Icon sx={{ pr: 4, pb: 3 }}>
+          <img alt="circular icon" src={icon} width="56px" height="56px" />
+        </Icon>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Button sx={styles.button}>{circularIcon()}</Button>
+    </div>
+  );
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,19 @@
 import { Grid } from "@mui/material";
+import CircularButton from "../../components/button/circular";
+import quicks from "../../assets/icon/quicks.svg";
 
 export default function Dashboard() {
   return (
     <div>
       <Grid
+        container
         sx={{
           fontFamily: "Lato",
           backgroundColor: "#262626",
           minHeight: "100vh",
         }}
       >
-        test file
+        <CircularButton icon={quicks} label={"text"} />
       </Grid>
     </div>
   );
