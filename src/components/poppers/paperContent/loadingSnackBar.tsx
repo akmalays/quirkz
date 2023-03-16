b8c560b3fbd9adbe53e1a26ab818e678db346d8a
akmalays <akmalays@gmail.com>
2023-03-16 12:04:09 +0700

    readjust loading snackbar styling

--- src/components/poppers/paperContent/loadingSnackBar.tsx
+++ src/components/poppers/paperContent/loadingSnackBar.tsx
@@ -3,16 +3,16 @@ import { CircularProgress, Grid, Typography } from "@mui/material";
 export default function LoadingSnackBar() {
   return (
     <div>
-      <Grid px={3} pb={1}>
+      <Grid px={3} pb={2}>
         <Grid
           sx={{
             backgroundColor: "#E9F3FF",
-            py: 1,
+            py: 1.5,
             px: 2,
             display: "flex",
             alignItems: "center",
-            gap: 3,
-            borderRadius: 2,
+            gap: 2,
+            borderRadius: 1,
           }}
         >
           <CircularProgress size="1.5rem" sx={{ size: "30px" }} />
